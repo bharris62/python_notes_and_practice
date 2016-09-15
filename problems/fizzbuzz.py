03eca9aed99af93b0fe056e65153143d872157a1
Andrew Harris <blakebharris@gmail.com>
2016-09-14 21:38:52 -0500

    fizzbuzz

---
+++ problems/fizzbuzz.py
@@ -0,0 +1,19 @@
+# Fizz buzz, using input().  Added a while loop that will keep the user in the game until they supply a valid integer.
+
+while True:
+    try:
+        n = int(input("Pick a number to be buzzed:  "))
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                print("FizzBuzz!")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+        break
+
+    except:
+        print("that is not a valid integer, try again.")
+
